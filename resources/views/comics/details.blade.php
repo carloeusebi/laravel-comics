1bf786fdb732d592a0771504f229d57f0b65cd72
carloeusebi <carloeusebi@gmail.com>
2023-08-21 16:31:16 +0200

    Added style to comic detail page

--- resources/views/comics/details.blade.php
+++ resources/views/comics/details.blade.php
@@ -1,35 +1,52 @@
 @extends('layouts.main')
 @section('scss')
     <link href="{{ Vite::asset('resources/scss/comics.details.scss') }}" rel="stylesheet" type="text/css">
+
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css'
+        integrity='sha512-Z0kTB03S7BU+JFU0nw9mjSBcRnZm2Bvm0tzOX9/OuOuz01XQfOpa0w/N9u6Jf2f1OAdegdIPWZ9nIZZ+keEvBw=='
+        crossorigin='anonymous' />
 @endsection
 
 @section('main-content')
-    <div class="container">
-        <div class="wrapper">
-            <div class="comic-details">
-                <h1>{{ $comic['title'] }}</h1>
-                <div class="price-and-avail">
-                    <div class="left">
-                        <div class="price">U.S Price: <span>{{ $comic['price'] }}</span></div>
-                        <div class="availability">Available</div>
+    <section id="comic-details">
+
+        <div class="container">
+            <div class="wrapper">
+                <div class="comic-details">
+                    <h1 class="uppercase">{{ $comic['title'] }}</h1>
+                    <div class="price-and-avail">
+                        <div class="left">
+                            <div>U.S Price: <span class="price">{{ $comic['price'] }}</span></div>
+                            <div class="availability">AVAILABLE</div>
+                        </div>
+                        <div class="right">
+                            <span>Check Availability</span>
+                            <i class="fas fa-caret-down"></i>
+                        </div>
                     </div>
-                    <div>Check Availability</div>
+                    <p class="description">{{ $comic['description'] }}</p>
                 </div>
-                <p>{{ $comic['description'] }}</p>
+                <figure class="advertisement">
+                    <figcaption>ADVERTISEMENT</figcaption>
+                    <img src="{{ Vite::asset('resources/img/test.jpg') }}" alt="advertisement">
+                </figure>
             </div>
-            <figure class="advertisement">
-                <figcaption>advertisement</figcaption>
-                <img src="{{ Vite::asset('resources/img/test.jpg') }}" alt="advertisement">
-            </figure>
         </div>
-        <div class="talent-and-specs">
+    </section>
+
+    <section id="talent-and-specs">
+        <div class="container">
+
             <div class="talent">
                 <h3>Talent</h3>
                 <div class="row">
                     <h4>Art by:</h4>
                     <div class="artists list">
                         @foreach ($comic['artists'] as $artist)
-                            <a href="#">{{ $artist }},</a>
+                            <a href="#">{{ $artist }}</a>
+                            @unless ($loop->last)
+                                ,
+                            @endunless
                         @endforeach
                     </div>
                 </div>
@@ -38,6 +55,9 @@
                     <div class="writers list">
                         @foreach ($comic['writers'] as $writer)
                             <a href="#">{{ $writer }}</a>
+                            @unless ($loop->last)
+                                ,
+                            @endunless
                         @endforeach
                     </div>
                 </div>
@@ -45,18 +65,18 @@
             <div class="specs">
                 <h3>Specs</h3>
                 <div class="row">
-                    <div>Series:</div>
-                    <div class="series"><a href="#">{{ $comic['series'] }}</a></div>
+                    <h4>Series:</h4>
+                    <div class="uppercase"><a href="#">{{ $comic['series'] }}</a></div>
                 </div>
                 <div class="row">
-                    <div>U.S. Price:</div>
+                    <h4>U.S. Price:</h4>
                     <div>{{ $comic['price'] }}</div>
                 </div>
                 <div class="row">
-                    <div>On Sale Date:</div>
+                    <h4>On Sale Date:</h4>
                     <div>{{ $comic['sale_date'] }}</div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
