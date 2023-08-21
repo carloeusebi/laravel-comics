09fbad7c35c508c62ab2d2a5cbabc0d08276d1c8
carloeusebi <carloeusebi@gmail.com>
2023-08-21 16:01:20 +0200

    Imported bolder font OS condensed

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,6 @@ import "./bootstrap";
 import "~resources/scss/app.scss";
 import "@fontsource/open-sans";
 import "@fontsource/open-sans-condensed/";
+import "@fontsource/open-sans-condensed/700.css";
 
 import.meta.glob(["../img/**"]);
